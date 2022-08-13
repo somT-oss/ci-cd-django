15defc2aa48d66bf4c92307e45b2e992ecc18d28
somT-oss <somtochukwuuchegbu@gmail.com>
2022-08-13 10:42:56 +0100

    add: added hello-world route

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_view(request):
+    html = "<h1>Hello World</h1>"
+    return HttpResponse(html)
